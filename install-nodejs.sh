bc4a223f8c61a8c2d56561f07c77f9f4b2902b09
Geo Joy <me.geojoy@gmail.com>
2016-10-25 13:46:29 +0530

    code update

--- install-nodejs.sh
+++ install-nodejs.sh
@@ -3,6 +3,8 @@
 sudo apt-get update
 
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.1/install.sh | bash
+export NVM_DIR=~/.nvm
+source ~/.nvm/nvm.sh
 nvm install stable
 
 node -v
